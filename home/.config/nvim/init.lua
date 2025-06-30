61b3738963af8952ae4b5d00874d3ee0d6abd0b0
Dan Pelensky <dan@pelensky.com>
2025-06-30 11:39:02 -0600

    June Updates

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,3 +1,4 @@
+-- ~/.config/nvim/init.lua
 vim.opt.runtimepath:prepend("~/.vim")
 vim.opt.runtimepath:append("~/.vim/after")
 vim.opt.packpath = vim.opt.runtimepath:get()
@@ -17,7 +18,7 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath
   })
 end
@@ -29,6 +30,7 @@ require("lazy").setup({
   { "scrooloose/nerdtree" },
   { "bling/vim-airline" },
   { "pivotal/tmux-config" },
+
   -- Linter
   {
     "dense-analysis/ale",
@@ -48,16 +50,7 @@ require("lazy").setup({
       vim.api.nvim_set_keymap('n', '<leader>af', ':ALEFix<CR>', { noremap = true, silent = false })
     end
   },
-  -- Language Specific
-  { "sheerun/vim-polyglot" },
-  { "evanleck/vim-svelte" },
-  -- Colours
-  {
-    "iammerrick/nova-vim",
-    config = function()
-      vim.cmd.colorscheme('nova')
-    end,
-  },
+
   -- TreeSitter
   {
     "nvim-treesitter/nvim-treesitter",
@@ -65,22 +58,32 @@ require("lazy").setup({
     event = "BufReadPost",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "css", "html", "javascript", "lua", "svelte", "typescript", },
+        ensure_installed = { "css", "html", "javascript", "lua", "svelte", "typescript" },
       })
     end,
   },
+
   -- Comments
   {
     "numToStr/Comment.nvim",
     config = function()
-      require('Comment').setup({
-      })
+      require('Comment').setup()
       vim.keymap.set('n', '<C-c>', '<Plug>(comment_toggle_linewise_current)')
       vim.keymap.set('x', '<C-c>', '<Plug>(comment_toggle_blockwise_visual)')
     end,
   },
+
+  -- Colours
+  {
+    "iammerrick/nova-vim",
+    config = function()
+      vim.cmd.colorscheme('nova')
+    end,
+  },
+
   -- Copilot
   { "github/copilot.vim" },
+
   -- FZF
   {
     "ibhagwan/fzf-lua",
@@ -90,17 +93,83 @@ require("lazy").setup({
         files = {
           cmd = "rg --files --hidden",
         },
-        vim.keymap.set("n", "<c-P>", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
       })
+      vim.keymap.set("n", "<C-P>", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
     end
   },
-  -- End of packages
+
+  -- LSP + Autocomplete
+  {
+"neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local cmp_lsp = require("cmp_nvim_lsp")
+
+      lspconfig.ts_ls.setup({
+        capabilities = cmp_lsp.default_capabilities(),
+        on_attach = function(_, bufnr)
+          local opts = { buffer = bufnr }
+
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+
+          -- Organize imports with `,oi`
+          vim.keymap.set("n", "<leader>oi", function()
+            vim.lsp.buf.code_action({
+              context = {
+                only = { "source.organizeImports" },
+                diagnostics = {},
+              },
+	      apply = true,
+            })
+	    -- Then run ALEFix
+	    vim.defer_fn(function()
+	      vim.cmd("ALEFix")
+	    end, 50)
+          end, opts)
+        end,
+        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+        root_dir = lspconfig.util.root_pattern("tsconfig.json", "package.json", ".git"),
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+      })
+    end,
+  },
 })
 
--- Disble mouse
+-- Disable mouse
 vim.opt.mouse = ""
 
--- Custom file type detection for .env files
+-- Filetype detection
 vim.cmd [[
   autocmd BufNewFile,BufRead .env.*  setf dosini
 ]]
